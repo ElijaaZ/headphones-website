52f1ea80493f23ce33ab213b4d1b332d8b9a27df
Elias Brunnberg <eliazbrunnberg@outlook.com>
2023-09-04 23:20:21 +0200

    changes

--- headphones-app-1/headphones-website/script.js
+++ headphones-app-1/headphones-website/script.js
@@ -1,3 +1,6 @@
+
+// För att få fram menyn i mobil läge
+
 const menuList = document.getElementById("menuList");
 menuList.style.maxHeight="0px";
 
@@ -10,3 +13,32 @@ function togglemenu(){
     }
 }
 
+// För att öka/minska antalet i varukorgen samt öka/minska priset
+
+const decrement = document.getElementById("decrement");
+const increment = document.getElementById("increment");
+const quantityNum = document.getElementById("quantity");
+const itemPrice = document.getElementById("item-price");
+const totalPrice = document.getElementById("total-price");
+
+let quantity = 1;
+let price = 999;
+
+function updateQuantity() {
+    quantityNum.textContent = `${quantity < 10 ? '0' : ''}${quantity}`;
+    totalPrice.textContent = (quantity * price) + " kr";
+}
+
+decrement.addEventListener("click", function() {
+    if (quantity > 1) {
+        quantity--;
+        updateQuantity();
+    }
+});
+
+increment.addEventListener("click", function() {
+    quantity++;
+    updateQuantity();
+});
+
+updateQuantity();
